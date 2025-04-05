e7d73a719bbdac69c375b8913dc6320cb7cca1e7
guerocanadiense81 <cedrickp1981@gmail.com>
2025-04-05 15:02:13 -0400

    Update mnemonic-eth.js

--- js/mnemonic-eth.js
+++ js/mnemonic-eth.js
@@ -1,6 +1,13 @@
-function generateMnemonic() {
-  const phrase = bip39.generateMnemonic();
+async function generateMnemonic() {
+  const bip39 = window.bip39;
+  const ethers = window.ethers;
+
+  const mnemonic = bip39.generateMnemonic();
+  const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+
   document.getElementById("outputBox").innerHTML = `
-    <p><strong>Mnemonic:</strong><br><code>${phrase}</code></p>
+    <p><strong>Mnemonic:</strong><br><textarea rows="2" readonly>${mnemonic}</textarea></p>
+    <p><strong>ETH Address:</strong><br><code>${wallet.address}</code></p>
+    <p><strong>Private Key:</strong><br><code>${wallet.privateKey}</code></p>
   `;
 }
