20914f1cc91029391f272367b88addb357b46824
guerocanadiense81 <cedrickp1981@gmail.com>
2025-04-05 12:33:29 -0400

    Update btc-gen.js

--- js/btc-gen.js
+++ js/btc-gen.js
@@ -1,2 +1,11 @@
-// btc-gen.js - full logic implemented here
-console.log('btc-gen ready');
+function generateBTC() {
+  const bitcoin = window.bitcoinjs;
+  const keyPair = bitcoin.ECPair.makeRandom();
+  const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
+  const wif = keyPair.toWIF();
+
+  document.getElementById("outputBox").innerHTML = `
+    <p><strong>Address:</strong> <code>${address}</code></p>
+    <p><strong>Private Key (WIF):</strong> <code>${wif}</code></p>
+  `;
+}
