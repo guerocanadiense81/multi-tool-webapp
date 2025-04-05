906403cd84bc7ee64276b34a44b38bcfe95fde89
guerocanadiense81 <cedrickp1981@gmail.com>
2025-04-05 14:04:40 -0400

    Update darkweb-dork.js

--- js/darkweb-dork.js
+++ js/darkweb-dork.js
@@ -1,2 +1,11 @@
-// darkweb-dork.js - full logic implemented here
-console.log('darkweb-dork ready');
+function generateDarkDork() {
+  const term = document.getElementById("dorkInput").value.trim();
+  const engines = [
+    `https://ahmia.fi/search/?q=${term}`,
+    `https://darksearch.io/search?query=${term}`,
+    `http://onion.pet/?q=${term}`,
+  ];
+
+  const out = engines.map(url => `<li><a href='${url}' target='_blank'>${url}</a></li>`).join("");
+  document.getElementById("outputBox").innerHTML = `<ul>${out}</ul>`;
+}
