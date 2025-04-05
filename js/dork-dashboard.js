fad72cf0b20ff7d6c24f6843899a012b074af94f
guerocanadiense81 <cedrickp1981@gmail.com>
2025-04-05 14:06:36 -0400

    Update dork-dashboard.js

--- js/dork-dashboard.js
+++ js/dork-dashboard.js
@@ -1,2 +1,15 @@
-// dork-dashboard.js - full logic implemented here
-console.log('dork-dashboard ready');
+const dorks = {
+  surface: ["inurl:login", "filetype:pdf confidential", "intitle:index.of"],
+  deep: ["site:pastebin.com password", "intext:'ssn' filetype:txt"],
+  dark: ["ahmia search onion", "inurl:.onion login", "bitcoin site:dark.fail"]
+};
+
+function loadDorks() {
+  const type = document.getElementById("typeSelect").value;
+  const list = dorks[type] || [];
+  document.getElementById("outputBox").innerHTML = list.map(d =>
+    `<li><a target='_blank' href='https://www.google.com/search?q=${encodeURIComponent(d)}'>${d}</a></li>`
+  ).join("");
+}
+
+window.onload = loadDorks;
