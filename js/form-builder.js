f10c5e2512cb01d3ce027754b1fa0862bbd5fb8f
guerocanadiense81 <cedrickp1981@gmail.com>
2025-04-05 13:58:04 -0400

    Update form-builder.js

--- js/form-builder.js
+++ js/form-builder.js
@@ -1,2 +1,11 @@
-// form-builder.js - full logic implemented here
-console.log('form-builder ready');
+function addInput() {
+  const div = document.createElement("div");
+  div.innerHTML = `<label>Label: <input type="text" name="field" placeholder="Enter..." /></label><br>`;
+  document.getElementById("formArea").appendChild(div);
+}
+
+function exportForm() {
+  const formArea = document.getElementById("formArea");
+  const output = formArea.innerHTML.replace(/<br>/g, "").trim();
+  document.getElementById("outputBox").value = `<form>\n${output}\n</form>`;
+}
